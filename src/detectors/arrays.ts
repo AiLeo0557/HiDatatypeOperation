126d17acdbe83ecc2d1c6ab305f89054fa140e76
AiLeo0557 <leo1949@dingtalk.com>
2025-03-07 11:30:41 +0800

    feat: 添加数组和元组类型检测函数注释

--- src/detectors/arrays.ts
+++ src/detectors/arrays.ts
@@ -1,6 +1,7 @@
 export const isArrayLike = (val: unknown): val is ArrayLike<unknown> =>
   val != null && typeof (val as any)[Symbol.iterator] === 'function';
 
+// 检测数组类型
 export const isUniformArray = <T>(
   arr: unknown,
   typeCheck: (val: unknown) => val is T
@@ -8,6 +9,7 @@ export const isUniformArray = <T>(
   return Array.isArray(arr) && arr.every(typeCheck);
 };
 
+// 检测元组类型，元组类型是长度固定的数组类型
 export const isTuple = <T extends unknown[]>(
   arr: unknown,
   ...typeChecks: { [K in keyof T]: (val: unknown) => val is T[K] }
